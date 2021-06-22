9472a4f207c395b3bbfeeb1b29a03b0f32c60186
Newton <narmstrongv@gmail.com>
2021-06-21 19:15:31 -0700

    added js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 function filterBYQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     let filteredResults = animalsArray;
@@ -62,6 +69,13 @@ app.get('/api/animals/:id', (req, res) => {
         
 });
 
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 app.listen(PORT, () => {
     console.log(`API sever now on ${PORT}!`);
 });
+
